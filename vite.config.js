ee6463b92ce96eb8084fb1a3108523a765d25527
Gerónimo Pericoli <geronimopericoli@gmail.com>
2023-07-05 20:43:50 -0300

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    extensions: ['.js', '.jsx'], // Agrega las extensiones de archivo que deseas resolver
+  },
+  build: {
+    rollupOptions: {
+      input: '/src/main.jsx', // Ruta al archivo de entrada principal
+    },
+  },
+});
