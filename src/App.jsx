78adad70c1cd621b8b7f079275c0a8898032ddea
Gerónimo Pericoli <geronimopericoli@gmail.com>
2023-07-06 17:37:46 -0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,10 @@ const predictA = async (inputText, setResult) => {
   setResult(result?.data);
 };
 
-const loadA = async (file, setResult) => {
-  const app = await client("https://pharmaia-pharmawise.hf.space/");
-
-  const fileReader = new FileReader();
-
-  fileReader.onload = async (event) => {
-    const jsonData = JSON.parse(event.target.result);
-    const result = await app.predict("/load_example", [jsonData]);
-    console.log(result?.data);
-    setResult(result?.data);
-  };
-
-  fileReader.readAsText(file);
-};
 
 function App() {
-  const [inputText, setInputText] = useState(null);
+  const [inputText, setInputText] = useState("");
   const [result, setResult] = useState(null);
-  const [selectedFile, setSelectedFile] = useState(null);
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (event) => {
@@ -42,13 +27,6 @@ function App() {
     setInputText(exampleText); // Establecer el contenido en el estado inputText
   };
 
-  const handleLoad = async (event) => {
-    event.preventDefault();
-    setLoading(true);
-    const file = event.target.elements.fileInput.files[0]; // Obtener el archivo seleccionado desde el nuevo campo de entrada
-    await loadA(file, setResult); // Pasar el archivo como primer argumento en loadA
-    setLoading(false);
-  };
 
   return (
     <>
@@ -94,18 +72,6 @@ function App() {
             {result}
           </div>
         )}
-
-        <span className="spacer"></span>
-        <form onSubmit={handleLoad}>
-          <input
-            className="input"
-            type="file"
-            name="fileInput"
-            onChange={(event) => setSelectedFile(event.target.files[0])}
-          />
-          <span className="spacer"></span>
-          <button type="submit">Cargar</button>
-        </form>
       </div>
     </>
   );
