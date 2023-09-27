67e359383e8250fa54090fbf7c830e5373e8a2e8
Gerónimo Pericoli <geronimopericoli@gmail.com>
2023-09-26 21:36:25 -0300

    last

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,59 @@ import React, { useState } from "react";
 import { client } from "@gradio/client";
 import "./App.css";
 
-const predictA = async (inputText, setResult) => {
-  const app = await client("https://pharma-ia-pharmawise-prospecto-megalabs.hf.space/");
-  const result = await app.predict("/predict", [inputText]);
-  setResult(result?.data);
-};
-
+// Import HfInferenceEndpoint from @huggingface/inference
+import { HfInferenceEndpoint } from '@huggingface/inference';
 
 function App() {
   const [inputText, setInputText] = useState("");
-  const [result, setResult] = useState(null);
+  const [result, setResult] = useState("");
   const [loading, setLoading] = useState(false);
+
+  // Define the streaming function
+  const streamTextGeneration = async () => {
+    // Create an instance of HfInferenceEndpoint with your endpoint URL and token
+    const hf = new HfInferenceEndpoint('https://YOUR_ENDPOINT.endpoints.huggingface.cloud', 'hf_YOUR_TOKEN');
+
+    // Define the prompt
+    const prompt = 'What can you do in Nuremberg, Germany? Give me 3 Tips';
+
+    // Start streaming text generation
+    const stream = hf.textGenerationStream({ inputs: prompt });
+
+    for await (const r of stream) {
+      // Update the result with the generated text
+      setResult((prevResult) => prevResult + r.token.text);
+    }
+  };
+
+  const predictA = async (inputText) => {
+    const app = await client("https://pharma-ia-pharmawise-prospecto-megalabs-v2-9.hf.space/");
+    const result = await app.predict("/predict", [inputText]);
+    setResult(result?.data);
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setLoading(true);
     setInputText(event.target.elements.inputText.value);
-    await predictA(inputText, setResult);
+    
+    // Call predictA function to get initial result
+    await predictA(inputText);
+
+    // Start streaming text generation
+    await streamTextGeneration();
+
     setLoading(false);
   };
 
   const handleExample = (event) => {
-    const exampleText = event.target.innerText; // Obtener el contenido de la etiqueta <span>
-    setInputText(exampleText); // Establecer el contenido en el estado inputText
+    const exampleText = event.target.innerText; 
+    setInputText(exampleText);
   };
-
 
   return (
     <>
       <h1>Api test</h1>
-
 
       <div className="card">
         <form onSubmit={handleSubmit}>
